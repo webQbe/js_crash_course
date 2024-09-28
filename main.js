37bd57034f5761d6e486f9bf2fdc0d7f00b1ed61
webQbe <webcubesolutionslk@gmail.com>
2024-09-28 12:33:53 +0530

    displayed alert and output messages to console

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('Hello World!');
+
+// output (print) to  console
+console.log('Hello World!');
+
+// output an error message
+console.error('This is an error!'); 
+
+// output a warning message
+console.warn('This is a warning!');
