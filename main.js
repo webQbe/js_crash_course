5bb7c7742b66ee49e2a0c179693135896b4ff640
webQbe <webcubesolutionslk@gmail.com>
2024-09-28 12:51:36 +0530

    variables: let and const

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
-alert('Hello World!');
+let age = 30;
+// change let value
+age = 31;
+console.log(age);
+
+
+let score;
+score = 10;
+console.log(score);
+
+/* const myscore;
+// you must add a value to const
+const myscore = 10;
+
+const myage = 30;
+// try to change const value
+myage = 31;
+console.log(age);
+// a const cannot be changed :Uncaught TypeError: invalid assignment to const 'myage' */
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* alert('Hello World!');
 
 // output (print) to  console
 console.log('Hello World!');
@@ -8,3 +49,6 @@ console.error('This is an error!');
 
 // output a warning message
 console.warn('This is a warning!');
+ */
+
+
