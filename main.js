0a69b7f7f03a207cb595ba4efc4d7d31bed5718c
webQbe <webcubesolutionslk@gmail.com>
2024-09-29 12:51:58 +0530

    Data Types explained

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-let age = 30;
+// Data Types
+// Strings, Numbers, Boolean, Null, Undefined
+
+const name = 'John'; 
+const age = 30; 
+const rating = 4.5; 
+const isCool = true;
+const x = null; // 
+const y = undefined; 
+let z; 
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof rating); // number
+console.log(typeof isCool); // boolean
+console.log(typeof x); // x is not an object, it's null 
+console.log(typeof y); // undefined
+console.log(typeof z); // undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let age = 30;
 // change let value
 age = 31;
 console.log(age);
@@ -6,7 +38,7 @@ console.log(age);
 
 let score;
 score = 10;
-console.log(score);
+console.log(score); */
 
 /* const myscore;
 // you must add a value to const
